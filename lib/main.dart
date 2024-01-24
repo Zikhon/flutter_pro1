0d97ca63ed9505451d031dc635855c6dd708285f
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-24 15:38:17 +0200

    Splash screeens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+// import 'package:flutter_pro1/screens/home.dart';
+import 'package:flutter_pro1/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,11 @@ class MyApp extends StatelessWidget {
   
     @override
     Widget build(BuildContext context) {
-      return MaterialApp(
+      return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('tryjkhs f'),
-        ),
-      ),
+        
+        home: SplashScreen(),
+    
       );
     }
   }
