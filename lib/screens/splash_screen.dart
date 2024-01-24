7177ec9ce1c17d28bd04ac8e26e64c9145e167d6
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-24 16:17:16 +0200

    ...

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,6 +10,47 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFF36E21), Color(0xFFF44336)],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft),
+        ),
+        child: Column(children: [
+          Icon(Icons.edit, size:80, color:Color(0xFFF44336))
+        ],),
+      ),
+    );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+
+// const Color primaryColor = Color(0xFF5722);
+// const Color secondaryColor = Color(0xFFCCBC);
+// const Color accentColor = Color(0x2e302f);
+// const Color errorColor = Color(0xfe2a39);
+
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: primaryColor,
+//       body: Container(
+//         decoration: BoxDecoration(
+//           color: primaryColor,
+//         ),
+//       ),
+//     );
+//   }
+// }
