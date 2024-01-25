9bad743ac1104a6e0558599d26b2ba868c9a69ba
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-25 12:46:02 +0200

    ....

--- lib/screens/mapscreen.dart
+++ lib/screens/mapscreen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-
-class MapScreen extends StatelessWidget {
-  const MapScreen({super.key});
+class MapScreen  extends StatefulWidget {
+  const MapScreen ({super.key});
 
   @override
+  State <MapScreen> createState() =>  _MapScreen_State();
+}
+
+class  _MapScreen_State extends State <MapScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Placeholder();
   }
 }
+ 
