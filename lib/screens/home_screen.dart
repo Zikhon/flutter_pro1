f318117db70a0bf8e23a5cd6826dec071c1b8ece
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-26 11:27:17 +0200

    Added input validation and error message

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,63 +12,104 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController latController = TextEditingController();
   TextEditingController lngController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-  backgroundColor: Color(0xFFCCBC),
-  leading: SizedBox(
-    height: 100,
-    width: 100,
-    child: Image.asset(
-      'Images/Quicloc8-logo.png',
-       fit: BoxFit.contain,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(0, 161, 56, 24),
+        leading: SizedBox(
+          height: 100,
+          width: 100,
+          
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Image.asset('Images/Quicloc8-logo.png',
+            fit: BoxFit.contain,
           ),
-       ),
-     ),
+          ]),
+      
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            'Where do you want to go?',
-            style: TextStyle(fontSize: 40),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextField(
-            controller: latController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'latitude',
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Where do you want to go?',
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: latController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'latitude',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: lngController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'longitude',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+            Container(
+  width: double.infinity,
+  margin: EdgeInsets.all(16.0),  // Add margin for spacing
+  child: ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      // Add style for the button
+      primary:Color(0xFFF36E21),// Background color
+      onPrimary: Colors.white,  // Text color
+    ),
+    onPressed: () {
+      if (isValidInput()) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => MapScreen(
+              double.parse(latController.text),
+              double.parse(lngController.text),
             ),
           ),
-          SizedBox(
-            height: 20,
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please enter valid latitude and longitude'),
+            backgroundColor:Color.fromARGB(255, 253, 8, 8),
           ),
-          TextField(
-            controller: lngController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'longitute',
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {
-                 Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => MapScreen(
-                          double.parse(latController.text),
-                          double.parse(lngController.text))));
-                },
-                child: Text('Get your destination!!')),
-          ),
-        ]),
+        );
+      }
+    },
+    child: Text('Get your destination!!'),
+  ),
+),
+
+          ],
+        ),
       ),
     );
+  }
+
+  bool isValidInput() {
+    try {
+      double.parse(latController.text);
+      double.parse(lngController.text);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
